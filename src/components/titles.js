91932663311d4060c60b2cf37edeadd620a29e43
Nico <nico@Nicos-MacBook-Pro.local>
2018-12-21 21:56:56 +0200

    add titles component

---
+++ src/components/titles.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Titles extends React.Component {
+
+    render() {
+
+        return (
+
+            <div>
+                <h1 className="title-container__ title">Weather Scanner </h1>
+                <p className="title-container__subtitle"> Helps you find Weather conditions in cities... </p>
+            </div>
+        )
+    }
+}
+
+export default Titles;
